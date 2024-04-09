94eb30066775dddba54b2ed1f673c5f5cf2ba745
Guilherme Viana <116905512+Guilherme0787@users.noreply.github.com>
2024-04-09 12:56:45 +0000

    screens commit

--- App.js
+++ App.js
@@ -1,59 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
+import { View, Text, ScrollView, Image, TouchableOpacity, Animated } from 'react-native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack"
 import { NavigationContainer } from '@react-navigation/native';
+import { styles } from  './styles'; //Importando os estilos do arquivo styles.js 
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
-      <TouchableOpacity onPress={()=>navigation.navigate("About")}>
-        <Text>Ir para conteudo</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+  const GuerradeToiaScreen=()=>{
+  const [imagemVisivel, setImagemVisivel] = useState(false);
+  const [mostrarArmas, setMostrarArmas] = useState(false);
+  const fadeAnim = useState(new Animated.Value(0))[0];
  
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Conteudo</Text>
-      <TouchableOpacity onPress={()=>navigation.navigate("About")}>
-        <Text>Ir para sobre</Text>
-      </TouchableOpacity>
-    </View>
-  );
-} 
-
-function AboutScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Sobre</Text>
-      <TouchableOpacity onPress={()=>navigation.goBack()}>
-        <Text>Voltar</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+   const toggleImagem = ()=>{
+    setImagemVisivel(!imagemVisivel);
+    Animated.timing(
+      fadeAnim,
+    {
+      toValue: imagemVisivel ? 0:1, //alterna entre 0  e 1
+      duration: 500, // Duração da animação em milissegundos
+      useNativeDriver: true, //Usa driver nativo para melhor performance
+   }
+  ).start();
+};
  
-const Stack = createNativeStackNavigator();
+return (
+  <View style={{ flex: 1}}>
+    <ScroolView contentContainerStyle={styles.scrollViewContainer}
+    style={{marginBottom:20}}>
+    <View style={styles.container}>
+<Text style={styles.titulo}>Guerra de Troia</Text>
+<Text style={styles.texto}>
+A Guerra de Troia foi um conflito lendário entre os gregos e os troianos por volta do século XII a.C.
+ Foi desencadeado pelo rapto da rainha grega Helena por Páris, príncipe troiano.
+  Durou dez anos e culminou na queda de Troia após os gregos usarem o Cavalo de Troia, um ardil para entrar na cidade.
+   Esta história é narrada na Ilíada e na Odisseia, atribuídas a Homero, e teve grande impacto na cultura ocidental.
+</Text>
+<TouchableOpacity onPress={toggleImagem} style={styles.botao}>
+  <text style={styles.textoBotao}>{imagemVisivel}</text>
+</TouchableOpacity>
 
-export default function App() {
-  return (
-    <NavigationContainer>
-    <Stack.Navigator>
-  <Stack.Screen name="Home" component={HomeScreen} />
-  <Stack.Screen name="Sobre" component={AboutScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
-  );
-}
+    
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
